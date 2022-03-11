c3eba947995e9cf1d07b67dc483b2a9ae241bac4
Natanael Copa <ncopa@alpinelinux.org>
2022-03-11 17:30:07 +0100

    Add unit tests using luaunit

--- test.lua
+++ test.lua
@@ -1,16 +1,18 @@
+local lu = require('luaunit')
 
-optarg = require("optarg")
+local optarg = require("optarg")
 
 
-opthelp = [[
+local opthelp = [[
 Options:
  -a                    A short option without long option
  -b DIRECTORY          A short option with required option DIRECTORY
  -h, --help            A simple option with long name
  -q, --quiet           Another simple option with long name
  -o, --outfile=FILE    An option with required optarg FILE
- -s, --without-optarg  This is similar -b but wihtout the 
+ -s, --without-optarg  This is similar -b but wihtout the
      --single-longopt  This is a longopt without shortopt
+                       but with multiline helptext
      --infile=FILE     This is a longopt with required optarg, without shortopt
 
 Note that leading space is required for options.
@@ -18,19 +20,37 @@ Option parsing is stopped at '--'.
 
 ]]
 
-opts, args = optarg.from_opthelp(opthelp)
-
-if not opts or opts.help then
-	print(("Usage: %s [-ahqs] [-b DIRECTORY] [-o FILE] [--] [FILE...]"):format(_G.arg[0]))
-	print(opthelp)
-	os.exit(opts and 1 or 0)
+function test_from_opthelp()
+	local tests = {
+		{argv={'-a'},					opts={a=1},						args={}},
+		{argv={'-a','-a','-a'},		opts={a=3},						args={}},
+		{argv={'-h', '--help'},		opts={h=2,help=2},			args={}},
+		{argv={'-b','ma','-a'},		opts={b='ma',a=1},			args={}},
+		{argv={'--outfile','m'},	opts={o='m', outfile='m'}, args={}},
+		{argv={'--outfile=m'},		opts={o='m', outfile='m'}, args={}},
+		{argv={'--infile','m'},		opts={infile='m'},			args={}},
+		{argv={'--infile=m'},		opts={infile='m'},			args={}},
+		{argv={'-h','OPT','-a'},	opts={h=1,help=1,a=1},		args={'OPT'}},
+		{argv={'-a','-a','--','-a','foo'},	opts={a=2},			args={'-a','foo'}},
+		{argv={'-s'},					opts={s=1,['without-optarg']=1},	args={}},
+		{argv={'--single-longopt'}, opts={['single-longopt']=1}, args={}},
+		{argv={'-ah'},					opts={a=1,h=1,help=1},		args={}},
+		{argv={'-ab','B', '-a'},	opts={a=2,b='B'},				args={}},
+	}
+	for _, t in pairs(tests) do
+		local opts, args = optarg.from_opthelp(opthelp, t.argv)
+		lu.assertEquals(opts, t.opts, "args failed: "..table.concat(t.argv,' '))
+		lu.assertEquals(args, t.args)
+	end
 end
 
-for k,v in pairs(opts) do
-	print ("option:", k,"value:",v)
+function test_missing_optarg()
+	local called=false
+	local opts, args = optarg.from_opthelp(opthelp, {'-b'}, function()
+		called=true
+	end)
+	lu.assertNil(opts)
+	lu.assertTrue(called)
 end
 
-for i =1, #args do
-	print(("args[%i]:\t%s"):format(i,args[i]))
-end
-
+os.exit(lu.LuaUnit.run())
